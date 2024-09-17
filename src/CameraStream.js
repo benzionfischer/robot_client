e2d3e57664373bcde4fc646ff878a79b38a525f6
Ben Zion Fischer <benzionfisher@gmail.com>
2024-09-17 17:25:32 +0300

    Put the camera at the currect place

--- src/CameraStream.js
+++ src/CameraStream.js
@@ -8,13 +8,15 @@ const CameraStream = () => {
         display: 'flex',
         justifyContent: 'center', // Centers horizontally
         alignItems: 'center',     // Centers vertically
-        height: '100vh',          // Full viewport height for vertical centering
+        height: '80vh',          // Full viewport height for vertical centering
+        position: 'relative',     // Enables relative positioning
+        top: '-50px',             // Moves the container 50px upward (adjust as needed)
       }}
     >
-      <div style={{ width: '100%', maxWidth: '500px' }}>
+      <div style={{ width: '100%', maxWidth: '900px' }}>
         <h2 style={{ textAlign: 'center' }}>Camera Stream</h2>
         <img
-          src="http://robot.local:5001/video_feed"
+          src="http://localhost:5001/video_feed"
           alt="Camera Feed"
           style={{
             width: '100%',          // Set the width to take the full available width of the parent
